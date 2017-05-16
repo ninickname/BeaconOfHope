4d566281c93ce0bd2a3b96b27af9f945e953e54c
Nick <yashaev@gmail.com>
2017-05-16 05:15:03 +0300

    bugfix

--- src/main/java/com/beaconofhope/controllers/rest/UserController.java
+++ src/main/java/com/beaconofhope/controllers/rest/UserController.java
@@ -1,6 +1,7 @@
 package com.beaconofhope.controllers.rest;
 
 import com.beaconofhope.models.User;
+import com.beaconofhope.repositories.UserRepository;
 import com.beaconofhope.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
@@ -10,16 +11,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 /**
  * Created by Nick on 11/02/2017.
  */
 
 
 @RestController
-@RepositoryRestController
-@RequestMapping("api/user/")
+@RequestMapping("/user/")
 public class UserController {
 
+    @Autowired
+    UserRepository userRepo;
+
+    @RequestMapping("getUsers")
+    public List<User> getUsers(){
+        return userRepo.findAll();
+    }
 
 
 
